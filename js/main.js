ad5ab6ba250602f155b7a23a20c836314483de23
Karen <karenmvaldivia@gmail.com>
2016-03-07 17:15:13 -0500

    dashgum

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* MODAL - LOGIN */
+// $(document).ready(function() {
+// 	$("#myModal").hide();
+	
+// 	$("#openBtn").click(function() {
+// 		$("#myModal").modal(toggle);
+// 	});
+	
+// 	$("body").click(function() {
+//     	$("#myModal").hide();
+// 	});
+// });
